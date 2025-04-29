32297f1ff6a1f0fc488f2148d258e984f322a57a
Leticia <leticiabraga10004@gmail.com>
2025-04-29 14:31:38 -0300

    mini projeto python

--- site.exemplo/index.py
+++ site.exemplo/index.py
@@ -1,7 +1,24 @@
- s = 0
-c = 0
-for c in range(3, 500, 3):
-    if c % 3 == 0:
-        print('{}'.format(c))
-    s +=c
-print('A soma de todos os números é:\033[1;32m {} \033[m'.format(s))
+somaidade=0
+mi=0
+nomevelho=0
+maioridadehomem=0
+totmulher20=0
+for k in range(1,3):
+    print('='*7, 'PESSOA {}'.format(k),'='*7)
+    nome = str(input('NOME:')).strip().capitalize()
+    idade = int(input('IDADE:'))
+    sexo = str(input('SEXO [F/M]:'))
+    somaidade += idade
+    if k == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    else:
+        if sexo in 'Mm' and idade > maioridadehomem:
+            maioridadehomem = idade
+            nomevelho = nome
+        elif sexo in 'Ff' and idade<20:
+            totmulher20 += 1
+print('O número de mulheres com menos de 20 anos é: {}'.format(totmulher20))
+print('O homem mais velho é {}, com {} anos'.format(nomevelho,maioridadehomem))
+mi+=somaidade/2
+print('A média das idades é: {}'.format(mi))
